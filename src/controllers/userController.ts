a0c84ca67263b8d611dc0cac664a2c9311dd0640
athar.ateeq <athar.ateeq@mergestack.com>
2024-07-11 15:07:27 +0500

    Built middleware and auth routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose'; 
-import _ from 'lodash';
+import { isNil, isEmpty, some } from 'lodash';
 import * as userService from '@services/userService';
 import { HTTP_STATUS } from '@utils/constants';
 import { MESSAGES } from '@utils/message';
 import { IUser } from '@models/User';
 import { sendResponse } from '@utils/Respons/response';
+import { AuthRequest } from 'types/auth-request';
 
 export const createUser = async (req: Request, res: Response) => {
   try {
@@ -16,7 +17,7 @@ export const createUser = async (req: Request, res: Response) => {
     }
     // Create user
     const user : IUser | null = await userService.createUser(req.body);
-    sendResponse(res, HTTP_STATUS.CREATED, MESSAGES.RECORD_ADDED_SUCCESSFULLY, user);
+    sendResponse(res, HTTP_STATUS.CREATED, MESSAGES.SIGNUP_SUCCESSFULLY, user);
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
       const errors = Object.values(error.errors).map((err) => err.message);
@@ -27,7 +28,7 @@ export const createUser = async (req: Request, res: Response) => {
   }
 };
 
-export const getUsers = async (req: Request, res: Response) => {
+export const getUsers = async (req: AuthRequest, res: Response) => {
   try {    
     const users : IUser[]= await userService.getUsers();
     sendResponse(res, HTTP_STATUS.OK, MESSAGES.RECORD_FETCHED_SUCCESSFULLY, users);
@@ -36,7 +37,7 @@ export const getUsers = async (req: Request, res: Response) => {
   }
 };
 
-export const getUserById = async (req: Request, res: Response) => {
+export const getUserById = async (req: AuthRequest, res: Response) => {
   try {
     const user : IUser | null = await userService.getUserById(req.params.userId);
     if (user) {
@@ -54,7 +55,7 @@ export const updateUser = async (req: Request, res: Response) => {
     const userData = req.body;
     const userId = req.params.id;
     // Check if userData object is empty or contains null/undefined values
-    if (_.isEmpty(userData) || _.some(userData, _.isNil)) {
+    if (isEmpty(userData) || some(userData, isNil)) {
       return sendResponse(res, HTTP_STATUS.BAD_REQUEST, MESSAGES.EMPTY_RECORD);
     }  
     // Check if user with the same email already exists
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const API_PREFIX = '/api';
 export const HTTP_STATUS = {
   OK: 200,
@@ -5,5 +8,8 @@ export const HTTP_STATUS = {
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
-  UNAUTHORIZED: 403,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
 };
+export const SECRET_KEY = process.env.SECRET_KEY ;
+export const TOKEN_EXPIRES_IN = process.env.TOKEN_EXPIRES_IN;
